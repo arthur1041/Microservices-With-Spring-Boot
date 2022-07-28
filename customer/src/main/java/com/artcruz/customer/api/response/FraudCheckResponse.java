5e89ff621732f9d899fe15564af340b54da199d1
Arthur Cruz <arthur.silva1041@gmail.com>
2022-07-28 02:43:25 -0300

    sending request to fraud service from customer service

--- customer/src/main/java/com/artcruz/customer/api/response/FraudCheckResponse.java
+++ customer/src/main/java/com/artcruz/customer/api/response/FraudCheckResponse.java
@@ -1,10 +1,9 @@
-package com.artcruz.fraud.api.response;
+package com.artcruz.customer.api.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
--- customer/src/main/java/com/artcruz/customer/service/CustomerService.java
+++ customer/src/main/java/com/artcruz/customer/service/CustomerService.java
@@ -1,16 +1,20 @@
 package com.artcruz.customer.service;
 
 import com.artcruz.customer.api.request.CustomerRegistrationRequest;
+import com.artcruz.customer.api.response.FraudCheckResponse;
 import com.artcruz.customer.model.repository.CustomerRepository;
 import com.artcruz.customer.model.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest req) {
         Customer customer = Customer.builder()
@@ -19,6 +23,16 @@ public class CustomerService {
                 .email(req.getEmail())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+            "http://localhost:8081/api/v1/fraud-check/{customerId}",
+            FraudCheckResponse.class,
+            customer.getId()
+        );
+
+        if(fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
