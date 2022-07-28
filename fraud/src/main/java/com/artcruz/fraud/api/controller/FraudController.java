aff21e21857f13dd4a8312cddbf75cef864381f6
Arthur Cruz <arthur.silva1041@gmail.com>
2022-07-28 15:53:35 -0300

    adding Slf4j on FraudController

--- fraud/src/main/java/com/artcruz/fraud/api/controller/FraudController.java
+++ fraud/src/main/java/com/artcruz/fraud/api/controller/FraudController.java
@@ -2,11 +2,13 @@ package com.artcruz.fraud.api.controller;
 
 import com.artcruz.fraud.api.response.FraudCheckResponse;
 import com.artcruz.fraud.service.FraudCheckService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public class FraudController {
 
     @Autowired
@@ -14,7 +16,11 @@ public class FraudController {
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID ) {
-        return new FraudCheckResponse(fraudCheckService.isFraudulentCustomer(customerID));
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+
+        log.info("fraud check request for customer {}", customerID);
+
+        return new FraudCheckResponse(isFraudulentCustomer);
     }
 
 }
